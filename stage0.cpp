c4131dac2173ce4f42e47aa9a7c100ed7819eafc
Trevor Smith <32603779+TheSivaSolution@users.noreply.github.com>
2022-10-30 17:13:38 -0500

    Update stage0.cpp

--- stage0.cpp
+++ stage0.cpp
@@ -1,1 +1,75 @@
-//just putting this here
+
+Compiler(char **argv) // constructor
+{
+   sourceFile.open(argv[1]);
+   listingFile.open(argv[2]);
+   objectFile.open(argv[3]);
+}
+
+~Compiler()           // destructor
+{
+   sourceFile.close();
+   listingFile.close();
+   objectFile.close();
+}
+
+void createListingHeader()
+{
+   time_t now = time (NULL);
+   listingFile << left << "STAGE0:\t" << "Trevor Smith" << ctime(&now) << endl;
+   listingFile << left << setw(15) "LINE NO:" << "SOURCE STATEMENT" << endl;
+   
+}
+
+void parser()
+{
+   
+}
+
+void createListingTrailer()
+{
+   
+}
+
+
+// Methods implementing the grammar productions
+void prog()           // stage 0, production 1
+{
+   
+}
+
+void progStmt()       // stage 0, production 2
+{
+   
+}
+
+void consts()         // stage 0, production 3
+{
+   
+}
+
+void vars()           // stage 0, production 4
+{
+   
+}
+
+void beginEndStmt()   // stage 0, production 5
+{
+   
+}
+
+void constStmts()     // stage 0, production 6
+{
+   
+}
+
+void varStmts()       // stage 0, production 7
+{
+   
+}
+
+string ids()          // stage 0, production 8
+{
+   
+}
+
